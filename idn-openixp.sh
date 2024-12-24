4feac5cc86f4216ee07be2416e78f6b26debf9d7
༺ Initial H ༻ <alsyundawy@gmail.com>
2024-12-25 04:33:02 +0700

    Update idn-openixp.sh

--- idn-openixp.sh
+++ idn-openixp.sh
@@ -1,5 +1,45 @@
 #!/bin/bash
 
+# Judul:  
+# Bash Script untuk Membuat File ACL Indonesia dengan Data IP dari Nice.rsc dan APNIC
+
+# Fungsi:  
+# Script ini digunakan untuk membuat file konfigurasi ACL (Access Control List) untuk Indonesia 
+# dengan menggabungkan entri IP lokal dan data IP yang diunduh dari dua sumber eksternal: Nice.rsc dan APNIC. 
+# Script ini secara otomatis mengunduh data, memprosesnya, dan menghasilkan file konfigurasi yang siap digunakan 
+# untuk pengaturan DNS atau firewall.
+
+# Deskripsi:  
+# 1. Inisialisasi URL dan Output:  
+#    - Mendefinisikan URL sumber file nice.rsc dan delegated-apnic-latest dari APNIC.
+#    - Menentukan lokasi output file konfigurasi ACL di /etc/bind/acl_indonesia.conf.
+
+# 2. Menambahkan Entri IP Lokal:  
+#    - Menambahkan entri untuk IP lokal yang umum digunakan seperti 10.0.0.0/8, 172.16.0.0/12, 192.168.0.0/16, dan lainnya.
+
+# 3. Mengunduh dan Memproses Data Nice.rsc:  
+#    - Mengunduh file nice.rsc dari URL yang telah ditentukan.
+#    - Menggunakan awk untuk mengekstrak alamat IP dan menambahkannya ke file output.
+
+# 4. Mengunduh dan Memproses Data APNIC:  
+#    - Mengunduh data delegated-apnic-latest dari APNIC.
+#    - Menggunakan awk untuk mengekstrak alamat IP dari data APNIC dan menambahkannya ke file output.
+
+# 5. Menghapus Duplikat dan Menjaga Konsistensi Format:  
+#    - Menggunakan awk untuk menghapus duplikat dan memastikan format file output tetap konsisten.
+
+# 6. Menutup Konfigurasi ACL:  
+#    - Menambahkan tanda penutup }; pada akhir file ACL.
+
+# 7. Output Informasi:  
+#    - Menampilkan pesan bahwa file ACL Indonesia berhasil dibuat.
+
+# Dibuat oleh:  
+# HARRY DS ALSYUNDAWY
+
+# Tanggal Dibuat:  
+# 25 Desember 2025
+
 # URL file sumber
 NICE_URL="http://ixp.mikrotik.co.id/download/nice.rsc"
 APNIC_URL="https://ftp.apnic.net/stats/apnic/delegated-apnic-latest"
